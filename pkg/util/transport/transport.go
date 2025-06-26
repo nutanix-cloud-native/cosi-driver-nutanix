d5af75f2f6b6ab937a2c254b5b6bc8ffc5b70a2c
ankush.patanwal <ankush.patanwal@nutanix.com>
2025-06-26 16:55:05 +0000

    Improved readability

--- pkg/util/transport/transport.go
+++ pkg/util/transport/transport.go
@@ -28,43 +28,49 @@ func BuildTransportTLS(tlsConfig TlsConfig) (*http.Transport, error) {
 		}
 
 		klog.InfoS("insecure connection made.", "insecure", tlsConfig.Insecure, "endpoint", tlsConfig.Endpoint)
-	} else {
-		if tlsConfig.CACert == "" {
-			transport = &http.Transport{
-				TLSClientConfig: &tls.Config{
-					InsecureSkipVerify: false,
-				},
-			}
-		} else {
-			var rootCAs []byte
-			if strings.Contains(tlsConfig.CACert, "-----BEGIN CERTIFICATE-----") && strings.Contains(tlsConfig.CACert, "-----END CERTIFICATE-----") {
-				rootCAs = []byte(tlsConfig.CACert)
-			} else {
-				// Decode base64 CA cert
-				_rootCAs, err := base64.StdEncoding.DecodeString(tlsConfig.CACert)
-				if err != nil {
-					return nil, fmt.Errorf("failed to decode CA cert: %v", err)
-				}
 
-				rootCAs = _rootCAs
-			}
+		return transport, nil
+	}
 
-			// Create cert pool and add our CA
-			caCertPool := x509.NewCertPool()
-			if !caCertPool.AppendCertsFromPEM(rootCAs) {
-				return nil, fmt.Errorf("failed to append CA cert: %s", tlsConfig.CACert)
-			}
-
-			transport = &http.Transport{
-				TLSClientConfig: &tls.Config{
-					RootCAs:            caCertPool,
-					InsecureSkipVerify: false,
-				},
-			}
+	if tlsConfig.CACert == "" {
+		transport = &http.Transport{
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: false,
+			},
 		}
 
-		klog.InfoS("secure connection made.", "insecure", tlsConfig.Insecure, "endpoint", tlsConfig.Endpoint)
+		klog.InfoS("secure connection made without CA certs.", "insecure", tlsConfig.Insecure, "endpoint", tlsConfig.Endpoint)
+
+		return transport, nil
 	}
+
+	var rootCAs []byte
+	if strings.Contains(tlsConfig.CACert, "-----BEGIN CERTIFICATE-----") && strings.Contains(tlsConfig.CACert, "-----END CERTIFICATE-----") {
+		rootCAs = []byte(tlsConfig.CACert)
+	} else {
+		// Decode base64 CA cert
+		_rootCAs, err := base64.StdEncoding.DecodeString(tlsConfig.CACert)
+		if err != nil {
+			return nil, fmt.Errorf("failed to decode CA cert: %v", err)
+		}
+
+		rootCAs = _rootCAs
+	}
+
+	// Create cert pool and add our CA
+	caCertPool := x509.NewCertPool()
+	if !caCertPool.AppendCertsFromPEM(rootCAs) {
+		return nil, fmt.Errorf("failed to append CA cert: %s", tlsConfig.CACert)
+	}
+
+	transport = &http.Transport{
+		TLSClientConfig: &tls.Config{
+			RootCAs:            caCertPool,
+			InsecureSkipVerify: false,
+		},
+	}
+
+	klog.InfoS("secure connection made with CA certs.", "insecure", tlsConfig.Insecure, "endpoint", tlsConfig.Endpoint)
 
 	return transport, nil
 }
